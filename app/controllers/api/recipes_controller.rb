bd54b7ece1327eb93e222cf16e57a8ddbe8b9c9b
brian <riceb53@gmail.com>
2020-03-05 22:02:13 -0500

    add recipes create action

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -9,4 +9,19 @@ class Api::RecipesController < ApplicationController
     @recipe = Recipe.find_by(id: the_id)
     render 'show.json.jb'
   end
+
+  def create
+    # make a new Recipe
+    @recipe = Recipe.new(
+      chef: params[:input_chef],
+      ingredients: params[:input_ingredients], 
+      directions: params[:input_directions], 
+      prep_time: params[:input_prep_time], 
+      image_url: params[:input_image_url], 
+      title: params[:input_title]
+    )
+    @recipe.save
+
+    render 'show.json.jb'
+  end
 end
