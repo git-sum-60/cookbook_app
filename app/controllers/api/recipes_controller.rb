570e8e8718d812c57ec27e2561c979b1a7f1c070
brian <riceb53@gmail.com>
2020-03-05 22:24:03 -0500

    add update action

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -24,4 +24,21 @@ class Api::RecipesController < ApplicationController
 
     render 'show.json.jb'
   end
+
+  def update
+    # find the correct recipe (show)
+    the_id = params[:id]
+    @recipe = Recipe.find_by(id: the_id)
+    # update the recipe (create)
+
+    @recipe.title = params[:input_title] || @recipe.title
+    @recipe.chef = params[:input_chef] || @recipe.chef
+    @recipe.ingredients = params[:input_ingredients] || @recipe.ingredients
+    @recipe.prep_time = params[:input_prep_time] || @recipe.prep_time
+    @recipe.directions = params[:input_directions] || @recipe.directions
+
+    @recipe.save
+
+    render 'show.json.jb'
+  end
 end
