290ac010ce9053a54b4d600fec818e51c059b03c
brian <riceb53@gmail.com>
2020-03-09 17:03:01 -0400

    add recipes destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,10 @@ Rails.application.routes.draw do
   #   get "/photos" => "photos#index"
   # end
   namespace :api do
-    get "/recipes" => "recipes#index"
+    get "/recipes" => "recipes#index" 
     get "/recipes/:id" => "recipes#show"
     post "/recipes" => "recipes#create"
     patch "/recipes/:id" => "recipes#update"
+    delete "/recipes/:id" => "recipes#destroy"
   end
 end
