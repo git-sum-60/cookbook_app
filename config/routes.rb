2ab8d8aaa75f4e138f64d87f9389cf1f767aec7f
brian <riceb53@gmail.com>
2020-03-17 17:20:43 -0400

    steps 1 and 2 of authentication cheatsheet

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,10 @@ Rails.application.routes.draw do
     post "/recipes" => "recipes#create"
     patch "/recipes/:id" => "recipes#update"
     delete "/recipes/:id" => "recipes#destroy"
+
+    post "/users" => "users#create"
+
+    post "/sessions" => "sessions#create"
+
   end
 end
