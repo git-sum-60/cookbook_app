ddb8f709590f3e2372ad5107b8a9ab03062f549b
brian <riceb53@gmail.com>
2020-03-25 19:19:37 -0400

    add links from index to show, and link to index in pplication.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  
   get "/recipes" => "recipes#index"
   get "/recipes/:id" => "recipes#show"
   
