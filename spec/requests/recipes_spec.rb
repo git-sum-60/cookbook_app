f8fb569a443a3bf6fc107a69ade731241c5f7f63
brian <riceb53@gmail.com>
2020-04-30 20:25:05 -0400

    add first test for index action

---
+++ spec/requests/recipes_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "Recipes", type: :request do
+  describe "GET /api/recipes" do
+    it "returns all the recipes in the database" do
+      # response = get "/api/recipes"
+      user = User.create!(email: "kerry@keryy.com", password: "password", name: "kary")
+
+      Recipe.create!(title: "thai curry", ingredients: "bottle of curry", directions: "pour the sauce on whatever", prep_time: 3, user_id: user.id)
+      
+      get "/api/recipes"
+
+      p JSON.parse(response.body)
+      
+      recipes = JSON.parse(response.body)
+      
+      expect(response).to have_http_status(200)
+      expect(recipes.length).to eq(1)
+    end
+  end
+end
